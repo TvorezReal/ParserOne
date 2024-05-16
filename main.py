bf7d08299a7ba03de01066a692070b4779ffc64d
TvorezReal <sentr777@bk.ru>
2024-05-16 08:12:46 +0300

    add new code

--- main.py
+++ main.py
@@ -1,53 +1,23 @@
-import requests
-from bs4 import BeautifulSoup
-from googletrans import Translator
+from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+import random
+import time
 
+browser = webdriver.Chrome()
+browser.get("https://ru.wikipedia.org/wiki/%D0%A1%D0%BE%D0%BB%D0%BD%D0%B5%D1%87%D0%BD%D0%B0%D1%8F_%D1%81%D0%B8%D1%81%D1%82%D0%B5%D0%BC%D0%B0")
 
-def get_russian_words():
-    url = "https://randomword.com/"
-    translator = Translator()
-    try:
-        r = requests.get(url)
-        soup = BeautifulSoup(r.content, "html.parser")
-        english_word = soup.find("div", id='random_word').text.strip()
-        word_definition = soup.find("div", id='random_word_definition').text.strip()
+hatnotes = []
+for element in browser.find_elements(By.TAG_NAME, "div"):
+    cl = element.get_attribute("class")
+    if cl == "hatnote navigation-not-searchable":
+        hatnotes.append(element)
 
-        # Перевод слова и определения на русский язык
-        russian_word = translator.translate(english_word, src='en', dest='ru').text
-        russian_definition = translator.translate(word_definition, src='en', dest='ru').text
+print(hatnotes)
+hatnote = random.choice(hatnotes)
+link = hatnote.find_element(By.TAG_NAME, "a").get_attribute("href")
 
-        return {'russian_word': russian_word,
-                'word_definition': russian_definition
-                }
-    except Exception as e:
-        print("Error Occurred:", e)
-        return None
+browser.get(link)
 
-
-def word_game():
-    print('Добро пожаловать в игру!')
-
-    while True:
-        word_dict = get_russian_words()
-        if word_dict is None:
-            print("Не удалось получить слово.")
-            return
-
-        word = word_dict['russian_word']
-        word_definition = word_dict['word_definition']
-        print(f'Определение слова: {word_definition}')
-        user_input = input('Какое это слово?: ')
-        if user_input.lower() == word.lower():
-            print('Верно!')
-        else:
-            print(f'Неправильно, правильное слово: {word}')
-
-        play_again = input('Хотите сыграть еще раз? (y/n): ')
-        if play_again.lower() != 'y':
-            print('Спасибо за игру!')
-            break
-
-
-word_game()
 
 
